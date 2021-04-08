07adfb88aed1ead951dacc416ecb8e14fde5f535
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-04-08 17:58:22 -0500

    Setting everything up

---
+++ PyPoll/main.py
@@ -0,0 +1,25 @@
+# Objectives
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
+
+import os
+import csv
+
+# Variables
+
+
+# Define the filepath
+# Could not do it as mentioned in class for some reason.
+dirname = os.path.dirname(__file__)  # Had to use this to generate a full path
+csvpath = os.path.join(dirname,'Resources', 'election_data.csv')
+
+# Reading using CSV module
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next (csvreader)
+
